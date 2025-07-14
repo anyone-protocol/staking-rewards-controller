31c22a7094450e6eabdc0c8f8d1cf7435a22766d
Jim Toth <jim@artby.city>
2025-07-14 11:02:02 -0400

    Use redis in sentinel mode in stage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { TasksModule } from './tasks/tasks.module'
 import { MongooseModule } from '@nestjs/mongoose'
@@ -9,6 +9,7 @@ import { ClusterModule } from './cluster/cluster.module'
 import { StakingRewardsModule } from './staking-rewards/staking-rewards.module'
 import { OperatorRegistryModule } from './operator-registry/operator-registry.module'
 import { BundlingModule } from './bundling/bundling.module'
+import { ConnectionOptions } from 'bullmq'
 
 @Module({
   imports: [
@@ -24,15 +25,63 @@ import { BundlingModule } from './bundling/bundling.module'
       inject: [ConfigService],
       useFactory: (
         config: ConfigService<{
+          REDIS_MODE: string
           REDIS_HOSTNAME: string
           REDIS_PORT: number
-        }>
-      ) => ({
-        connection: {
+          REDIS_MASTER_NAME: string
+          REDIS_SENTINEL_1_HOST: string
+          REDIS_SENTINEL_1_PORT: number
+          REDIS_SENTINEL_2_HOST: string
+          REDIS_SENTINEL_2_PORT: number
+          REDIS_SENTINEL_3_HOST: string
+          REDIS_SENTINEL_3_PORT: number
+        }>,
+      ) => {
+        const logger = new Logger(AppModule.name)
+        const redisMode = config.get<string>(
+          'REDIS_MODE',
+          'standalone',
+          { infer: true }
+        )
+
+        let connection: ConnectionOptions = {
           host: config.get<string>('REDIS_HOSTNAME', { infer: true }),
           port: config.get<number>('REDIS_PORT', { infer: true }),
-        },
-      }),
+        }
+
+        if (redisMode === 'sentinel') {
+          const name = config.get<string>('REDIS_MASTER_NAME', { infer: true })
+          const sentinels = [
+            {
+              host: config.get<string>(
+                'REDIS_SENTINEL_1_HOST',
+                { infer: true }
+              ),
+              port: config.get<number>('REDIS_SENTINEL_1_PORT', { infer: true })
+            },
+            {
+              host: config.get<string>(
+                'REDIS_SENTINEL_2_HOST',
+                { infer: true }
+              ),
+              port: config.get<number>('REDIS_SENTINEL_2_PORT', { infer: true })
+            },
+            {
+              host: config.get<string>(
+                'REDIS_SENTINEL_3_HOST',
+                { infer: true }
+              ),
+              port: config.get<number>('REDIS_SENTINEL_3_PORT', { infer: true })
+            }
+          ]
+          connection = { sentinels, name }
+        }
+
+        logger.log(`Connecting to Redis with mode ${redisMode}`)
+        logger.log(`Connection: ${JSON.stringify(connection)}`)
+
+        return { connection }
+      }
     }),
     BundlingModule,
     DistributionModule,
